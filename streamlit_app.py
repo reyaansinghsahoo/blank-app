508241bc62ba748091b83c40db9d8d77706ae5af
reyaansinghsahoo <reyaansinghsahoo@gmail.com>
2024-08-03 10:58:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ st.write(
 )
 st.write("1*1=")
 st.write(1*1)
+number1=st.number_input("please insert number 1")
+number2=st.number_input("please insert number 2")
+st.write(number1*number2)
+                        
 
 
 
