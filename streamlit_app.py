d12605aeefc0dbabc7651d2290b5d4a888bafd30
reyaansinghsahoo <reyaansinghsahoo@gmail.com>
2024-08-09 11:31:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,62 +2,58 @@ import streamlit as st
 import numpy as np
 
 if st.button("Refresh"):
-          st.rerun()
+  st.cache_resource.clear()
+  st.rerun()
 
-st.title("this is a pop quiz for 7 and over.")
+def check_answer(a,s):
+  if a==s:
+    st.write( " YAY YOU ARE CORRRRRRRREEEEEECCCCCCTTTTTTTTTTTTTTTTTT")
+  else:
+    st.write(" YOU GET A RED CAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRD")
 
-n1 =np.random.randint(low = 0,high = 9)
+st.title("This is a maths Sats test")
+n1=np.random.randint(low=0,high=9)
 n2=np.random.randint(low=0,high=9)
 s=n1+n2
-st.write("first number is ",n1)
-st.write("second number is",n2)
-#st.write("the sum is", s)
-a=st.number_input("enter your answer",step=1)
 
-if st.button(" chek your anwser"):
-if a==s:
-          st.write(" yay all thogh you are not smart you have got it corect")
-else:
-          st.write ("you are dumb")
-          
+if 'answer' not in st.session_state:
+  # st.write("inside session creation:")
+  st.session_state.answer = s
+  # st.write("session answer:",s)
+
+
+st.write("First number is " , n1)
+st.write("Second number is " , n2)
+# st.write("The first sum is" , s)      
+a=st.number_input(" Enter your answer",step=1)
+
+
+if st.button("check your answer"):
+  ans = st.session_state.answer
+  st.write("Actual Answer: ", ans)
+  st.write("Your Answer: ", a)
+  if a==ans:
+    st.write( " YAY YOU ARE CORRRRRRRREEEEEECCCCCCTTTTTTTTTTTTTTTTTT")
+  else:
+    st.write(" YOU GET A RED CAAAAAAAAARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRD")
+  st.session_state.answer = s
+                 
 
 st.divider()
-st.title("reyaans first app")
-          
-st.write("this is my first app i will make a calculator")
-st.write("1*1=")
-st.write(1*1)
-number1=st.number_input("please insert number 1", step=1)
-number2=st.number_input("please insert number 2", step=1)
 
-st.write("the sum is")
+
+
+st.title("Rishaan first app ")
+st.write("This is my first app, I will make a calculator")
+st.write (" 10*100000")
+st.write( 10*100000)
+
+number1=st.number_input("Insert number 1",step=1)
+number2=st.number_input("Insert number 2",step=1)
+st.write("the sum is ")
 st.write(number1+number2)
-st.write("the product is")
+st.write("the product is ")
 st.write(number1*number2)
-st.write("the difference is")
+st.write("the difference")
 st.write(number1-number2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         
+st.write(number1-number2)
